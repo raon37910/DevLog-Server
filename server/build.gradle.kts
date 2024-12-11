d1d5db7525ae9b0eadb610e21c9c48bd7d29df3e
김정민 <raon37910@gmail.com>
2024-12-11 10:56:14 +0900

    기본 프로젝트 세팅

---
+++ server/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.raon"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // MVC
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // JDBC
+//    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    // MySQL
+//    runtimeOnly("com.mysql:mysql-connector-j")
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ server/src/test/java/com/raon/devlog/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raon.devlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
