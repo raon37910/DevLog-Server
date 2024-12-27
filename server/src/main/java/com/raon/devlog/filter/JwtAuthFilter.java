f66101971522b6f529fe81ca25c7726caea3c854
김정민 <raon37910@gmail.com>
2024-12-27 14:33:16 +0900

    스프링 시큐리티 Role Based Authentication 추가

---
+++ server/src/main/java/com/raon/devlog/filter/JwtAuthFilter.java
@@ -0,0 +1,48 @@
+package com.raon.devlog.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.raon.devlog.domain.auth.JwtTokenProvider;
+import com.raon.devlog.service.auth.model.TokenClaim;
+
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	public JwtAuthFilter(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		String authorizationHeader = request.getHeader("Authorization");
+		if (StringUtils.isNotBlank(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+			String accessToken = authorizationHeader.substring(7);
+			TokenClaim tokenClaim = jwtTokenProvider.parseToken(accessToken);
+			List<SimpleGrantedAuthority> authorities = tokenClaim.roles()
+				.stream()
+				.map(SimpleGrantedAuthority::new)
+				.toList();
+
+			Authentication authentication = new TestingAuthenticationToken(tokenClaim.subject(), "password",
+				authorities);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+
+		filterChain.doFilter(request, response);
+	}
+}
