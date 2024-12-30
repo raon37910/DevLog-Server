0de21be1fef54dc4d48f568905b62c921be883c0
김정민 <raon37910@gmail.com>
2024-12-30 14:14:24 +0900

    회원 가입 API 리팩토링

---
+++ server/src/main/java/com/raon/devlog/repository/user/UserQuery.java
@@ -0,0 +1,39 @@
+package com.raon.devlog.repository.user;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.raon.devlog.mapper.user.RoleEntity;
+import com.raon.devlog.mapper.user.UserEntity;
+import com.raon.devlog.mapper.user.UserMapper;
+import com.raon.devlog.support.error.DevlogException;
+import com.raon.devlog.support.error.ErrorType;
+
+@Repository
+public class UserQuery {
+	private final UserMapper userMapper;
+
+	public UserQuery(UserMapper userMapper) {
+		this.userMapper = userMapper;
+	}
+
+	public boolean existsByEmail(String email) {
+		return userMapper.existsByEmail(email);
+	}
+
+	public void ifExistsByEmailDoThrow(String email) {
+		if (existsByEmail(email)) {
+			throw new DevlogException(ErrorType.VALIDATION_ERROR);
+		}
+	}
+
+	public Optional<UserEntity> findByEmail(String email) {
+		return Optional.ofNullable(userMapper.findByEmail(email));
+	}
+
+	public List<RoleEntity> getRoles(Long userId) {
+		return userMapper.getRoles(userId);
+	}
+}
